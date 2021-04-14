8070e9a3ac093e448944bebdf662e7d6bc8626de
Thomas Schouten <hollandpirates@gmail.com>
2021-04-14 14:47:26 +0200

    Remove incorrect authentication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 
-    val kotlinVersion = "1.5.0-M2"
+    val kotlinVersion = "1.4.32"
 
     application
     kotlin("jvm") version kotlinVersion
@@ -12,49 +12,25 @@ plugins {
     // Plugin which can update Gradle dependencies, use help/useLatestVersions
     id("se.patrikerdes.use-latest-versions") version "0.2.16"
 
-    kotlin("plugin.serialization") version "1.5.0-M2"
+    kotlin("plugin.serialization") version "1.4.32"
 
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    // To "prevent strange errors".
-    implementation(kotlin("reflect"))
-    // Kotlin reflection.
-    implementation(kotlin("test"))
-    implementation(kotlin("test-junit"))
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:1.0-M1-1.4.0-rc-218")
     implementation("com.charleskorn.kaml:kaml:0.30.0")
-
     implementation("org.islandoftex.arara:arara-api:6.1.0")
     implementation("org.islandoftex.arara:arara-cli:6.1.0")
     implementation("org.islandoftex.arara:arara-mvel:6.1.0")
     implementation("org.islandoftex.arara:arara-core:6.1.0")
-
-    // Transitive dependencies
-    implementation("com.soywiz.korlibs.korio:korio:2.0.9")
     implementation("org.slf4j:slf4j-api:1.7.30")
-    implementation("io.github.microutils:kotlin-logging:2.0.6")
 }
 
 repositories {
-    // Based on https://docs.gitlab.com/ee/user/packages/maven_repository/#authenticate-to-the-package-registry-with-maven
     maven {
-        // As shown on GitLab
         url = uri("https://gitlab.com/api/v4/projects/14349047/packages/maven")
-        // Also not working:
-//        url = uri("https://gitlab.com/api/v4/projects/1512074/packages/maven")
-//        url = uri("https://gitlab.com/api/v4/groups/org.islandoftex.arara/-/packages/maven")
-        name = "GitLab"
-        credentials(HttpHeaderCredentials::class) {
-            name = "Private-Token"
-            // From a gradle.properties file
-            value = properties["gitLabPrivateToken"] as String
-        }
-        authentication {
-            create<HttpHeaderAuthentication>("header")
-        }
     }
     mavenCentral()
     mavenLocal()
@@ -63,7 +39,8 @@ repositories {
 
 tasks.compileKotlin {
     kotlinOptions {
-        languageVersion = "1.5"
-        apiVersion = "1.5"
+        languageVersion = "1.4"
+        apiVersion = "1.4"
+        jvmTarget = "11"
     }
 }
--- src/main/kotlin/nl/deltadak/ktemplate/AraraDeserializeRuleExample.kt
+++ src/main/kotlin/nl/deltadak/ktemplate/AraraDeserializeRuleExample.kt
@@ -1,11 +1,11 @@
 package nl.deltadak.ktemplate
 
-import org.islandoftex.arara.api.files.MPPPath
 import org.islandoftex.arara.cli.ruleset.RuleUtils
 import java.io.File
+import java.nio.file.Path
 
 fun main() {
-    val file = MPPPath(File("src/main/resources/arara-rule-sage.yaml").absolutePath)
-    val rule = RuleUtils.parseYAMLRule(file, "sage")
+    val file = Path.of(File("src/main/resources/arara-rule-sage.yaml").absolutePath)
+    val rule = RuleUtils.parseRule(file, "sage")
     println(rule.arguments.first())
 }
